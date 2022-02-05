c6095a1d650552666f34c0de94dc955baa8edce6
442009190 <91487767+442009190@users.noreply.github.com>
2022-02-05 10:30:17 +0300

    Create ClassOOP.java

---
+++ ClassOOP.java
@@ -0,0 +1,35 @@
+public class item{
+ String title 
+ String description
+ public void viewDdescription(){}
+}
+
+public class DvD{
+ String director 
+ String cirtificate 
+ public void viewDdescription(){}
+}
+
+public class CD{
+ String artist 
+ String ganre 
+ String numberOftracks
+ public void viewDdescription(){}
+}
+
+public class Book{
+ String author   
+ String isbn  
+ public void viewDdescription(){}
+}
+
+public class Library{
+ String items    
+ public void viewDdescription(){}
+ public void  addItems(){}
+ public void  removeItems(){}
+ public void  listItems(){}
+ public void  numberOfItems(){}
+ public void  reset(){}
+  public void  isEmpty(){}
+}
